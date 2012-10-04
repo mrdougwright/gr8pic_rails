b2eff829fe7a3e3b1efa2d3783044b80d5932e1d
Doug Wright <mrdougwright@gmail.com>
2012-10-03 20:49:35 -0700

    added all rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gr8picRails::Application.config.secret_token = 'c883776dac2099c3efe4df7eb6f8f4ea7e70a06c474555b2d4342391107a5eb0a0a2bd7c344ad7a63588047bdfd955b6d215c0de3fc0425ca4b6effafc2a0266'
